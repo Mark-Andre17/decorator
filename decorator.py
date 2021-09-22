66a7b19d0542b2bbfbb1d14bd793cb304c994c7b
Mark-Andre17 <84619111+Mark-Andre17@users.noreply.github.com>
2021-09-23 00:27:40 +0300

    Initial commit

---
+++ decorator.py
@@ -0,0 +1,30 @@
+import datetime
+import os
+
+
+def get_path(file_path, file_name):
+    file_place = os.path.join(file_path, file_name)
+
+    def decorate_log(func):
+        def save_file(*args, **kwargs):
+            date = datetime.datetime.now()
+            name_func = func.__name__
+            result = func(*args, **kwargs)
+            with open(f'{file_name}', 'w', encoding='utf-8') as file:
+                file.write(
+                    f'Дата и время:{date}\nИмя функции:{name_func}\nАргументы функции:{args, kwargs}\nРезудьтат '
+                    f'функции:{result}\nПуть к файлу:{file_place}')
+            return result
+
+        return save_file()
+
+    return decorate_log
+
+
+@get_path('pythonProject1', 'logger.txt')
+def count_numbers(a, b):
+    return a+b
+
+
+if __name__ == '__main__':
+    count_numbers(3, 5)
